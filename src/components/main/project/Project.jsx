8366996c2f6a574c804d33dfbadb2b9e66db7fba
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2022-10-09 00:44:54 +0700

    [CHG] set data project and call in jsx

--- src/components/main/project/Project.jsx
+++ src/components/main/project/Project.jsx
@@ -1,109 +1,30 @@
 import React from "react";
 import "./Project.css";
+import Projects from "../../../data/Project";
 
 function Project() {
-  const BASE_URL = "https://alfanwahyudi.github.io";
-
+  const project = Projects.reverse().map((project) => {
+    return (
+      <div class="card">
+        <article class="project">
+          <h3 class="title_project">{project.title}</h3>
+          <p class="description">{project.description}</p>
+          <button class="btn btn_project">
+            <a href={project.url} target="_blank" rel="noreferrer">
+              Go to App
+            </a>
+          </button>
+        </article>
+      </div>
+    );
+  });
+  
   return (
     <section id="projects">
       <div class="container">
         <h2>Projects</h2>
         <div class="list_projects">
-          <div class="row">
-            <div class="card">
-              <article class="project">
-                <h3 class="title_project">Rock Paper Scissor</h3>
-                <p class="description">
-                  I'm create simple game rock paper scissor. I only use HTML,
-                  CSS, and JavaScript. I hope you love it 😙
-                </p>
-                <button class="btn btn_project">
-                  <a
-                    href="https://portfolio-rps.netlify.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Go to App
-                  </a>
-                </button>
-              </article>
-            </div>
-            <div class="card">
-              <article class="project">
-                <h3 class="title_project">Twitter Clone</h3>
-                <p class="description">
-                  It's only one page of home page twitter, only using HTML and
-                  CSS. Still have work to do...
-                </p>
-                <button class="btn btn_project">
-                  <a
-                    href={`${BASE_URL}/twitter-clone`}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    Go to App
-                  </a>
-                </button>
-              </article>
-            </div>
-          </div>
-          <div class="row">
-            <div class="card">
-              <article class="project">
-                <h3 class="title_project">Restauroka</h3>
-                <p class="description">
-                  We make the finding restaurant base on website it will helpul
-                  to find restaurant near us
-                </p>
-                <button class="btn btn_project">
-                  <a
-                    href={`${BASE_URL}/restauroka`}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    Go to App
-                  </a>
-                </button>
-              </article>
-            </div>
-            <div class="card">
-              <article class="project">
-                <h3 class="title_project">Movie Finder</h3>
-                <p class="description">
-                  Have movie data from The Movie Database (TMDB) that you can
-                  easily find
-                </p>
-                <button class="btn btn_project">
-                  <a
-                    href={`${BASE_URL}/movie_finder`}
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    Go to App
-                  </a>
-                </button>
-              </article>
-            </div>
-          </div>
-          <div class="row">
-            <div class="card">
-              <article class="project">
-                <h3 class="title_project">Ask Tech</h3>
-                <p class="description">
-                  Two main feature for ask about hardware and show tech news
-                </p>
-                <button class="btn btn_project">
-                  <a
-                    href="https://play.google.com/store/apps/details?id=com.unikom.asktech"
-                    rel="noreferrer"
-                    target="_blank"
-                  >
-                    Go to App
-                  </a>
-                </button>
-              </article>
-            </div>
-          </div>
+          {project}
         </div>
         <div class="divider"></div>
       </div>
---
+++ src/data/Project.js
@@ -0,0 +1,34 @@
+const Projects = [
+  {
+    id: 1,
+    title: "Ask Tech",
+    url: "https://play.google.com/store/apps/details?id=com.unikom.asktech",
+    description: "Two main feature for ask about hardware and show tech news"
+  },
+  {
+    id: 2,
+    title: "Movie Finder",
+    url: "https://alfanwahyudi.github.io/movie_finder",
+    description: "Have movie data from The Movie Database (TMDB) that you can easily find"
+  },
+  {
+    id: 3,
+    title: "Restauroka",
+    url: "https://alfanwahyudi.github.io/",
+    description: "We make the finding restaurant base on website it will helpfull to find restaurant near us"
+  },
+  {
+    id: 4,
+    title: "Twitter Clone",
+    url: "https://alfanwahyudi.github.io/twitter-clone",
+    description: "It's only one page of home page twitter, only using HTML and CSS. Still have work to do..."
+  },
+  {
+    id: 5,
+    title: "Rock Paper Scissor",
+    url: "https://portfolio-rps.netlify.app/",
+    description: "I'm create simple game rock paper scissor. I only use HTML, CSS, and JavaScript. I hope you love it 😙"
+  },
+]
+
+export default Projects;
