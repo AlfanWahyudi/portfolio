44fec7d5eec8cfca5344adb83afe31089fbc77eb
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2022-10-14 20:23:26 +0700

    [UPD] send email is working

--- src/components/main/contact/Contact.jsx
+++ src/components/main/contact/Contact.jsx
@@ -1,36 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 import './Contact.css';
 
 function Contact() {
+  const EMAIL_JS_SERVICE_ID = "service_portfolio_alfan";
+  const EMAIL_JS_TEMPLATE_ID = "portfolio";
+  const EMAIL_JS_PUBLIC_KEY = "9MI9ak1AMsDFcfmaB";
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(EMAIL_JS_SERVICE_ID, EMAIL_JS_TEMPLATE_ID, form.current, EMAIL_JS_PUBLIC_KEY)
+      .then((result) => console.log(result.text))
+      .catch((error) => console.log(error.text))
+  }
+
   return (
     <section id="contact">
-      <div class="container">
+      <div className="container">
         <h2>Contact</h2>
-        <div class="contact_item">
-          <div class="detail_contact">
-            <div class="middle">
-              <div class="contact_icon">
-                <i class="fa-solid fa-envelope"></i>
+        <div className="contact_item">
+          <div className="detail_contact">
+            <div className="middle">
+              <div className="contact_icon">
+                <i className="fa-solid fa-envelope"></i>
               </div>
-              <a href="?">alfan.wahyudi98@gmail.com</a>
+              <a href="mailto:alfan.wahyudi98@gmail.com">alfan.wahyudi98@gmail.com</a>
             </div>
-            <div class="middle">
-              <div class="contact_icon">
-                <i class="fa-solid fa-location-dot"></i>
+            <div className="middle">
+              <div className="contact_icon">
+                <i className="fa-solid fa-location-dot"></i>
               </div>
               <span>Sukabumi, Jawa Barat Indonesia</span>
             </div>
           </div>
-          <form action="" method="post">
-            <div class="row">
-              <div class="col">
-                <label for="email" class="form_label">
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="hidden" name="to_name" value="Alfan Wahyudi" />
+            <div className="row">
+              <div className="col">
+                <label htmlFor="email" className="form_label">
                   Email
                 </label>
                 <input type="email" id="email" name="email" required />
               </div>
-              <div class="col">
-                <label for="message" class="form_label">
+              <div className="col">
+                <label htmlFor="message" className="form_label">
                   Message
                 </label>
                 <textarea
@@ -40,7 +55,7 @@ function Contact() {
                   rows="10"
                 ></textarea>
               </div>
-              <button class="btn btn_form" type="submit">
+              <button className="btn btn_form" type="submit">
                 Send
               </button>
             </div>
