b7393e127641ab5d7aad93dfc49d2c5b9c69f04e
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2023-06-01 18:58:49 +0700

    update styling sosmed alignment footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,46 +1,48 @@
 import React from "react";
 import './Footer.css';
-import boxArrowUpIcon from '../../assets/icons/box-arrow-up-right.svg';
-import githubIcon from '../../assets/icons/github.svg';
-import twitterIcon from '../../assets/icons/twitter.svg';
-import linkedinIcon from '../../assets/icons/linkeind.svg';
-import igIcon from '../../assets/icons/instagram.svg';
+import boxArrowUp from '../../assets/icons/box-arrow-up-right.svg';
+import github from '../../assets/icons/github.svg';
+import twitter from '../../assets/icons/twitter.svg';
+import linkedin from '../../assets/icons/linkeind.svg';
+import ig from '../../assets/icons/instagram.svg';
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
       <div className="container container__contact">
         <section id="contact" className="contact">
             <h2>Get In Touch</h2>
-            <p className="contact-email">
+            <a href="mailto:alfan.wahyudi98@gmail.com" className="contact-email" title="alfan.wahyudi98@gmail.com">
               <u>alfan.wahyudi98@gmail.com</u>  
-              <img src={boxArrowUpIcon} className="email-img" alt="sending email icon" />
-            </p>
+              <img src={boxArrowUp} className="email-img" alt="sending email icon" />
+            </a>
             <ul className="contact-social-media">
               <li className="contact-social-media-item">
-                <a href="/">
-                  <img src={githubIcon} alt="github icon" />
+                <a href="https://github.com/AlfanWahyudi" title="https://github.com/AlfanWahyudi">
+                  <img src={github} alt="github icon" />
                 </a>
               </li>
               <li className="contact-social-media-item">
-                <a href="/">
-                  <img src={twitterIcon} alt="twitter icon" />
+                <a href="https://twitter.com/Wahyudi_Alfann" title="https://twitter.com/Wahyudi_Alfann">
+                  <img src={twitter} alt="twitter icon" />
                 </a>
               </li>
               <li className="contact-social-media-item">
-                <a href="/">
-                  <img src={linkedinIcon} alt="linkedin icon" />
+                <a href="https://www.linkedin.com/in/alfanwahyudi" title="https://www.linkedin.com/in/alfanwahyudi">
+                  <img src={linkedin} alt="linkedin icon" />
                 </a>
               </li>
               <li className="contact-social-media-item">
-                <a href="/">
-                  <img src={igIcon} alt="instagram icon" />
+                <a href="https://www.instagram.com/alfan.id/" title="https://www.instagram.com/alfan.id">
+                  <img src={ig} alt="instagram icon" />
                 </a>
               </li>
             </ul>
         </section>
         <section id="copyRight" className="copy-right">
-          <p>copyright Alfan Wahyudi </p>
+          <p>&#169; {currentYear} Alfan Wahyudi </p>
         </section>
       </div>
     </footer>
