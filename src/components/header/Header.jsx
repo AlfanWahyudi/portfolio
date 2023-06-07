dc56533e796bef8e62cfe9864d23918842532bb9
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2023-06-07 20:48:42 +0700

    update styling header responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -19,31 +19,32 @@ function Header() {
         <button className="btn-menu" onClick={toggleMenu}>
           <img src={menuOpen ? close : menu} className="btn__menu-img" alt="Menu icon" />
         </button>
-          <nav className={`navbar ${menuOpen && 'open'}`}>
-            <ul className="nav">
-              <li className="nav-item">
-                <a href="#home" className="nav-link">Home</a>
-              </li>
-              <li className="nav-item">
-                <a href="#projects" className="nav-link">Projects</a>
-              </li>
-              <li className="nav-item">
-                <a href="#contact" className="nav-link">Contact</a>
-              </li>
-            </ul>
-            <ul className="social-media">
-              <li className="social-media-item">
-                <a href="/" className="social-media-link" target="blank">
-                  <img src={git} className="social-media-img" alt="github icon" />
-                </a>
-              </li>
-              <li className="social-media-item">
-                <a href="/" className="social-media-link" target="blank">
-                  <img src={linkedin} className="social-media-img" alt="linkedin icon" />
-                </a>
-              </li>
-            </ul>
-          </nav>
+        <nav className={`navbar ${menuOpen ? 'open' : ''}`}>
+          <ul className="nav">
+            <li className="nav-item">
+              <a href="#home" className="nav-link">Home</a>
+            </li>
+            <li className="nav-item">
+              <a href="#projects" className="nav-link">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact" className="nav-link">Contact</a>
+            </li>
+          </ul>
+          <div className="divider__vertical-header"></div>
+          <ul className="social-media">
+            <li className="social-media-item">
+              <a href="/" className="social-media-link" target="blank">
+                <img src={git} className="social-media-img" alt="github icon" />
+              </a>
+            </li>
+            <li className="social-media-item">
+              <a href="/" className="social-media-link" target="blank">
+                <img src={linkedin} className="social-media-img" alt="linkedin icon" />
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
