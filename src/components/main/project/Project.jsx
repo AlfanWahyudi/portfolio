9911eeacfbb05e7046ed48a6b70357d76b413c00
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2023-06-10 14:52:37 +0700

    update show data projects to web

--- src/components/main/project/Project.jsx
+++ src/components/main/project/Project.jsx
@@ -3,37 +3,40 @@ import "./Project.css";
 import Projects from "../../../data/Project";
 import github from '../../../assets/icons/github.svg';
 import boxArrowUpWhite from '../../../assets/icons/box-arrow-up-right-white.svg';
-import rpsGameImg from '../../../assets/images/rps-game.png';
 
 function Project() {
+  const projects = Projects.reverse().map((project) => {
+    return (
+      <article className="card-project">
+        <img src={project.img} className="project-img" alt="project img thumbnail" />
+        <div className="project-desc">
+          <h3 className="project-title">{project.title}</h3>
+          <p>{project.description}</p>
+          <section className="project-actions">
+            <button className="btn btn-link btn__project">
+              <a href={project.githubUrl} target="__blank" >
+                <img src={github} alt="github icon" />
+                Code
+              </a>
+            </button>
+            <button className="btn btn-link btn-link__primary btn__project">
+              <a href={project.url} target="__blank">
+                <img src={boxArrowUpWhite} alt="box arrow up icon" />  
+                Live Site
+              </a>
+            </button>
+          </section>
+        </div>
+      </article>
+    );
+  });
+  
   return (
     <section id="projects">
       <div className="container container__project">
         <h2>Projects</h2>
         <div className="list_projects">
-          <article className="card-project">
-            <img src={rpsGameImg} className="project-img" alt="project img thumbnail" />
-            <div className="project-desc">
-              <h3 className="project-title">Lorem Title</h3>
-              <p>Lorem Ipsum is simply dummy text of the printing and typesetting 
-                industry. Lorem Ipsum has been the industry's standard dummy text</p>
-              <section className="project-actions">
-                <button className="btn btn-link btn__project">
-                  <a href="/">
-                    <img src={github} alt="github icon" />
-                    Code
-                  </a>
-                </button>
-                <button className="btn btn-link btn-link__primary btn__project">
-                  <a href="/">
-                    <img src={boxArrowUpWhite} alt="box arrow up icon" />  
-                    Live Site
-                  </a>
-                </button>
-              </section>
-            </div>
-            <div></div>
-          </article>
+          {projects}
         </div>
       </div>
     </section>
