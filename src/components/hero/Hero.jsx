5ae29a1db30328be8be85587d6d13df6f990fb2c
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2023-06-18 15:27:07 +0700

    add CV & styling hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import './Hero.css';
+import cv from '../../assets/file/cv.pdf';
 
 function Hero() {
   return(
     <section id="hero">
       <div className="container container__hero">
         <hgroup className="hero-hgroup">
-          <h1 className="hero-hgroup-title">Hello, I'm Alfan Wahyudi</h1>
-          <p className="hero-hgroup-subhead">Saya adalah seorang programmer, dan sekarang bekerja sebagai Junior Programmer di PT Akhdani Reka Solusi.
-            Saya suka membuat website yang user friendly, dengan memperhatikan dari segi flow agar user tidak kebingungan.
-            Tools yang sering digunakan ialah JavaScript, PHP, Vite, React, Laravel, MySQL.</p>
+          <h1 className="hero-hgroup-title">Hi, I am Alfan Wahyudi</h1>
+          <p className="hero-hgroup-subhead">I'm a computer science graduate with programming and software development skills based in Indonesia.
+            Tools that are often used are JavaScript, PHP, Webpack, React, Laravel, and MySQL.</p>
         </hgroup>
         <div className="hero-cta">
           <button className="btn btn-link btn-link__primary-hover">
             <a href="#contact">Contact</a>
           </button>
           <button className="btn btn-link">
-            <a href="/">Download CV</a>
+            <a href={cv} target="__blank">Download CV</a>
           </button>
         </div>
       </div>
